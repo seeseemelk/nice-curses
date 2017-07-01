cd9533a09176eedcaaf5b4c8326cb92f0c07ae90
Michail Pevnev <mpevnev@gmail.com>
2017-07-01 21:08:51 +0000

    get{ch,str} functions and key table.

--- source/nice/curses.d
+++ source/nice/curses.d
@@ -285,9 +285,17 @@ final class Window
             }
         }
 
+        /* This one moves the cursor. */
         void move(int y, int x)
         {
-            if (mvwin(ptr, y, x) != OK) 
+            if (wmove(ptr, y, x) != OK) 
+                throw new NCException("Failed to move to position %s:%s", y, x);
+        }
+
+        /* This one moves the window. */
+        void moveWindow(int y, int x)
+        {
+            if (mvwin(ptr, y, x) != OK)
                 throw new NCException("Failed to move a window to position %s:%s", y, x);
         }
 
@@ -343,6 +351,14 @@ final class Window
                 throw new NCException("Failed to add character '%s' at %s:%s", ch, y, x);
         }
 
+        void addch(C: chtype, A: chtype)(C ch, A attr = Attr.normal)
+        {
+            import std.format;
+            setAttr(attr);
+            if (nc.waddch(ptr, ch) != OK)
+                throw new NCException("Failed to add character '%s'", ch);
+        }
+
         void addstr(A: chtype)(int y, int x, string str, A attr = Attr.normal)
         {
             import std.string;
@@ -352,12 +368,30 @@ final class Window
                 throw new NCException("Failed to write string '%s' at %s:%s", str, y, x);
         }
 
-        void addstr(A: chtype)(int y, int x, int n, string str, A attr = Attr.normal)
+        void addstr(A: chtype)(string str, A attr = Attr.normal)
+        {
+            import std.string;
+
+            setAttr(attr);
+            if (nc.waddstr(ptr, str.toStringz) != OK)
+                throw new NCException("Failed to write string '%s'", str);
+        }
+
+        void addnstr(A: chtype)(int y, int x, string str, int n, A attr = Attr.normal)
         {
             import std.string;
 
             setAttr(attr);
             if (nc.mvwaddnstr(ptr, y, x, str.toStringz, n) != OK)
+                throw new NCException("Failed to write string '%s' at %s:%s", str, y, x);
+        }
+
+        void addnstr(A: chtype)(int n, string str, int n, A attr = Attr.normal)
+        {
+            import std.string;
+
+            setAttr(attr);
+            if (nc.waddnstr(ptr, str.toStringz, n) != OK)
                 throw new NCException("Failed to write string '%s' at %s:%s", str, y, x);
         }
 
@@ -368,11 +402,46 @@ final class Window
         }
 
         /* ---------- information retrieval ---------- */
+
+        int getch()
+        {
+            int res = wgetch(ptr);
+            if (res == ERR)
+                throw new Exception("Failed to get a character");
+            return res;
+        }
+
+        string getstr(int maxLength)
+        {
+            import std.string;
+
+            char[] buffer = new char[maxLength + 1];
+            char* p = &buffer[0];
+            if (wgetstr(ptr, p) != OK)
+                throw new Exception("Failed to get a string");
+            /* The cast is safe, it's the only reference to the string. */
+            return cast(string) fromStringz(p);
+        }
 }
 
 final class ColorTable
 {
 
+}
+
+/* An exception that is thrown on ncurses errors. */
+class NCException: Exception 
+{
+    import std.format;
+    this(Arg...)(string formatString, Arg args)
+    {
+        super(format(formatString, args));
+    }
+}
+
+struct RGB
+{
+    short r, g, b;
 }
 
 enum Attr
@@ -397,19 +466,165 @@ enum Attr
     vertical = A_VERTICAL,
 }
 
-struct RGB
+enum Key
 {
-    short r, g, b;
-}
+    codeYes   = KEY_CODE_YES,
+    min       = KEY_MIN,
+    codeBreak = KEY_BREAK, /* This should've been break, but that's a keyword. */
 
-/* ---------- Exceptions ---------- */
-
-/* An exception that is thrown on ncurses errors. */
-class NCException: Exception 
-{
-    import std.format;
-    this(Arg...)(string formatString, Arg args)
-    {
-        super(format(formatString, args));
-    }
+    down      = KEY_DOWN,
+    up        = KEY_UP,
+    left      = KEY_LEFT,
+    right     = KEY_RIGHT,
+    home      = KEY_HOME,
+    backspace = KEY_BACKSPACE,
+    f0        = KEY_F0,
+    f1        = KEY_F(1),
+    f2        = KEY_F(2),
+    f3        = KEY_F(3),
+    f4        = KEY_F(4),
+    f5        = KEY_F(5),
+    f6        = KEY_F(6),
+    f7        = KEY_F(7),
+    f8        = KEY_F(8),
+    f9        = KEY_F(9),
+    f10       = KEY_F(10),
+    f11       = KEY_F(11),
+    f12       = KEY_F(12),
+    f13       = KEY_F(13),
+    f14       = KEY_F(14),
+    f15       = KEY_F(15),
+    f16       = KEY_F(16),
+    f17       = KEY_F(17),
+    f18       = KEY_F(18),
+    f19       = KEY_F(19),
+    f20       = KEY_F(20),
+    f21       = KEY_F(21),
+    f22       = KEY_F(22),
+    f23       = KEY_F(23),
+    f24       = KEY_F(24),
+    f25       = KEY_F(25),
+    f26       = KEY_F(26),
+    f27       = KEY_F(27),
+    f28       = KEY_F(28),
+    f29       = KEY_F(29),
+    f30       = KEY_F(30),
+    f31       = KEY_F(31),
+    f32       = KEY_F(32),
+    f33       = KEY_F(33),
+    f34       = KEY_F(34),
+    f35       = KEY_F(35),
+    f36       = KEY_F(36),
+    f37       = KEY_F(37),
+    f38       = KEY_F(38),
+    f39       = KEY_F(39),
+    f40       = KEY_F(40),
+    f41       = KEY_F(41),
+    f42       = KEY_F(42),
+    f43       = KEY_F(43),
+    f44       = KEY_F(44),
+    f45       = KEY_F(45),
+    f46       = KEY_F(46),
+    f47       = KEY_F(47),
+    f48       = KEY_F(48),
+    f49       = KEY_F(49),
+    f50       = KEY_F(50),
+    f51       = KEY_F(51),
+    f52       = KEY_F(52),
+    f53       = KEY_F(53),
+    f54       = KEY_F(54),
+    f55       = KEY_F(55),
+    f56       = KEY_F(56),
+    f57       = KEY_F(57),
+    f58       = KEY_F(58),
+    f59       = KEY_F(59),
+    f60       = KEY_F(60),
+    f61       = KEY_F(61),
+    f62       = KEY_F(62),
+    f63       = KEY_F(63),
+    dl        = KEY_DL,
+    il        = KEY_IL,
+    dc        = KEY_DC,
+    ic        = KEY_IC,
+    eic       = KEY_EIC,
+    clear     = KEY_CLEAR,
+    eos       = KEY_EOS,
+    eol       = KEY_EOL,
+    sf        = KEY_SF,
+    sr        = KEY_SR,
+    npage     = KEY_NPAGE,
+    ppage     = KEY_PPAGE,
+    stab      = KEY_STAB,
+    ctab      = KEY_CTAB,
+    catab     = KEY_CATAB,
+    enter     = KEY_ENTER,
+    sreset    = KEY_SRESET,
+    reset     = KEY_RESET,
+    print     = KEY_PRINT,
+    ll        = KEY_LL,
+    a1        = KEY_A1,
+    a3        = KEY_A3,
+    b2        = KEY_B2,
+    c1        = KEY_C1,
+    c3        = KEY_C3,
+    btab      = KEY_BTAB,
+    beg       = KEY_BEG,
+    cancel    = KEY_CANCEL,
+    close     = KEY_CLOSE,
+    command   = KEY_COMMAND,
+    copy      = KEY_COPY,
+    create    = KEY_CREATE,
+    end       = KEY_END,
+    exit      = KEY_EXIT,
+    find      = KEY_FIND,
+    help      = KEY_HELP,
+    mark      = KEY_MARK,
+    message   = KEY_MESSAGE,
+    move      = KEY_MOVE,
+    next      = KEY_NEXT,
+    open      = KEY_OPEN,
+    options   = KEY_OPTIONS,
+    previous  = KEY_PREVIOUS,
+    redo      = KEY_REDO,
+    reference = KEY_REFERENCE,
+    refresh   = KEY_REFRESH,
+    replace   = KEY_REPLACE,
+    restart   = KEY_RESTART,
+    resume    = KEY_RESUME,
+    save      = KEY_SAVE,
+    sbeg      = KEY_SBEG,
+    scancel   = KEY_SCANCEL,
+    scommand  = KEY_SCOMMAND,
+    scopy     = KEY_SCOPY,
+    screate   = KEY_SCREATE,
+    sdc       = KEY_SDC,
+    sdl       = KEY_SDL,
+    select    = KEY_SELECT,
+    send      = KEY_SEND,
+    seol      = KEY_SEOL,
+    sexit     = KEY_SEXIT,
+    sfind     = KEY_SFIND,
+    shelp     = KEY_SHELP,
+    shome     = KEY_SHOME,
+    sic       = KEY_SIC,
+    sleft     = KEY_SLEFT,
+    smessage  = KEY_SMESSAGE,
+    smove     = KEY_SMOVE,
+    snext     = KEY_SNEXT,
+    soptions  = KEY_SOPTIONS,
+    sprevious = KEY_SPREVIOUS,
+    sprint    = KEY_SPRINT,
+    sredo     = KEY_SREDO,
+    sreplace  = KEY_SREPLACE,
+    sright    = KEY_SRIGHT,
+    srsume    = KEY_SRSUME,
+    ssave     = KEY_SSAVE,
+    ssuspend  = KEY_SSUSPEND,
+    sundo     = KEY_SUNDO,
+    suspend   = KEY_SUSPEND,
+    undo      = KEY_UNDO,
+    mouse     = KEY_MOUSE,
+    resize    = KEY_RESIZE,
+    event     = KEY_EVENT,
+    max       = KEY_MAX,
 }
